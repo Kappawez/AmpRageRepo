cb1ec469c7d5604f5410e7fb6cf2375d5844e5c1
Nick Lindström <nicklindstrom1337@gmail.com>
2019-09-10 10:08:24 +0200

    AddedCarClass

---
+++ AmpRageRepo/Models/Car.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmpRageRepo
+{
+    class Car
+    {
+        public string? LicensePlate { get; set; }
+        public string Brand { get; set; }
+        public string Make { get; set; }
+        public decimal Capacity { get; set; }
+        public decimal ZeroToHundred { get; set; }
+        public int TopSpeed { get; set; }
+        public int Range { get; set; }
+        public decimal Efficiency { get; set; }
+        public int Fastcharge { get; set; }
+
+        public List<string> GetAllProps()
+        {
+            var listOfProps = new List<string>();
+
+            listOfProps.Add(this.Brand);
+            listOfProps.Add(this.Make);
+            listOfProps.Add(this.Capacity.ToString());
+            listOfProps.Add(this.ZeroToHundred.ToString());
+            listOfProps.Add(this.TopSpeed.ToString());
+            listOfProps.Add(this.Range.ToString());
+            listOfProps.Add(this.Efficiency.ToString());
+            listOfProps.Add(this.Fastcharge.ToString());
+
+            return listOfProps;
+        }
+    }
+}
