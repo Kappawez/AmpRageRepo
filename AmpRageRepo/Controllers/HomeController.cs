47e8baab34a17578db2129e1a9a7b30cfbe2fe58
micke_kappa@hotmail.com <micke_kappa@hotmail.com>
2019-09-10 13:55:03 +0200

    Sökruta, medmer

--- AmpRageRepo/Controllers/HomeController.cs
+++ AmpRageRepo/Controllers/HomeController.cs
@@ -24,6 +24,10 @@ namespace AmpRageRepo.Controllers
         {
             return View();
         }
+        public IActionResult SearchByReg()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
